e76468a5082edb2521f6240a72e11c20ddd5ffa0
anth-volk <anth.volk@gmail.com>
2023-02-13 12:26:12 +0000

    feat: user authentication

--- number_guess.sh
+++ number_guess.sh
@@ -2,7 +2,7 @@
 
 # Declare constants
 MAX_VALUE=1000
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 # Generate random number between 1 and $1 arg
 function _generateRandomInt() {
@@ -12,13 +12,35 @@ function _generateRandomInt() {
 
 # Authentication function, where $1 is username
 function _authenticateUser() {
-  local authOutput=$($PSQL "SELECT user_id FROM 
+
+  # Declare var that represents $1
+  local username=$1
+
+  # Attempt to locate user using input username
+  local authOutput=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$username';")
+
+  # If not able to find in DB, indicate that it is user's first time
+  if [[ -z $authOutput ]]
+  then
+    createUser=$($PSQL "INSERT INTO users(username) VALUES('$username');")
+
+    # Were this to not be for an FCC challenge (the tests will probably fail if implemented),
+    # here is where the script would check if insertion failed because username
+    # must be a unique value
+
+    echo "Welcome, $username! It looks like this is your first time here."
+  else
+    echo $authOutput | while IFS="|" read userId gamesPlayed bestGame
+    do
+      echo "Welcome back, $username! You have played $gamesPlayed games, and your best game took $bestGame guesses."
+    done
+  fi
 
 
 }
 
 # Ask user to enter username
-  # If not able to find in DB, indicate that it is user's first time
+  
 
   # Otherwise, print account data
 
