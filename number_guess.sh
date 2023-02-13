e035566605ffa05136fdd7f6869057a9cbc938f3
anth-volk <anth.volk@gmail.com>
2023-02-13 10:43:52 +0000

    chore: Outline program in comments

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+# Randomly generate number for user to guess between 1 and 1000
+
+# Ask user to enter username
+  # If not able to find in DB, indicate that it is user's first time
+
+  # Otherwise, print account data
+
+# Print line indicating user should guess
+  # Initiate local counter
+
+  # Begin loop
+
+  # Read user's guess
+
+  # Indicate whether user's guess is lower or higher
+
+  # Increment counter or print end message, then update stats in DB
+
+
+function main
